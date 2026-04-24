595c5c0aa51a2bb95a745bc484b85abd8b6c1ee4
Samuel Chueh (闕思恩) <117357209+ky4tz@users.noreply.github.com>
2026-04-24 15:57:27 +0800

    Create hooks.py

---
+++ custom_pos/hooks.py
@@ -0,0 +1,11 @@
+app_name = "custom_pos"
+app_title = "Custom POS"
+app_publisher = "Your Name"
+app_description = "Adds Custom Fields to POS"
+app_email = "ky4tz@icloud.com"
+app_license = "mit"
+
+# Inject Custom JS into the POS Page
+page_js = {
+    "point-of-sale": "public/js/pos_override.js"
+}
